d9b385e445631d6a30581ff3b59dd747216388d1
AJIT <AJIT@192.168.0.3>
2016-06-23 21:58:16 +0530

    file is updated

--- WebContent/js/tab.js
+++ WebContent/js/tab.js
@@ -1,4 +1,15 @@
+
 function loadPortfolioPage(){
 	
-	$("tablist").load("html/portfolio.html");
+	
+	$("#tablist").load("html/portfolio.html");
+}
+
+function loadRegistrationForm(){
+	 $("#tablist").load("html/registrationForm.html");
+}
+
+function loadHomePage(){
+	
+	$("#tablist").load("html/homepage.html");
 }
