10290f66d644fdfbb5e3647cbf7e4533b664cf53
vin <vinay.volcan96@gmail.com>
2016-07-02 15:21:52 +0530

    SignUp Controller

--- WebContent/js/signupcontroller.js
+++ WebContent/js/signupcontroller.js
@@ -13,3 +13,36 @@ function hideSignUpModal(){
 		});
 	
 }
+
+function signUpUser(){
+    if($("#sign_psw").val()==$("#sign_conf_psw").val()){
+    	$.ajax({
+            url: "actorservlet?action=register",
+            method: "POST",
+            data:  $('#signup_form').serialize(),
+            dataType: 'JSON',
+            success: function(data) {
+              	if(data.check){
+              		$("#signupmodal").modal("hide");
+               		$("#signupmodal").on('hidden.bs.modal', function (event) {
+               			if ($('.modal:visible').length) //check if any modal is open
+               			{
+               				$('body').addClass('modal-open');//add class to body
+               			}
+               		});
+                }
+                else{
+                		alert('This email id is already registered')
+                		$("#errordetails_signup").text("This email id is already registered").delay(3000).fadeOut();
+                }
+            },
+            error: function(data) {
+            	alert('Unknown Error Occured');
+            }
+        });
+    }else{
+    	alert('not correct')
+    	$("#errordetails_signup").text("Password and confirm password fields are different...!").delay(3000).fadeOut();
+    }   
+	
+}
