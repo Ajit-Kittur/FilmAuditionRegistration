011feb5eb5d60b74fa6ada3eccb00e33b0aa1c27
AJIT <AJIT@AJIT-HP>
2016-07-16 18:48:08 +0530

    admin signup modal and admin forgot password modal is updated

--- WebContent/js/adminlogincontroller.js
+++ WebContent/js/adminlogincontroller.js
@@ -8,3 +8,42 @@ function adminloginUser(){
 	alert("sucessfully logged")
 	
 }
+
+function showSignUpModal(){
+	$("#myModal3").modal("hide");
+	$("#myModal3").on('hidden.bs.modal', function (event) {
+		  if ($('.modal:visible').length) //check if any modal is open
+		  {
+		    $('body').addClass('modal-open');//add class to body
+		  }
+		});
+
+	$("#myModal6").modal("show");
+	
+}
+
+function hideAdminSignUpModal(){
+	$("#myModal6").modal("hide");
+	$("#myModal6").on('hidden.bs.modal', function (event) {
+		  if ($('.modal:visible').length) //check if any modal is open
+		  {
+		    $('body').addClass('modal-open');//add class to body
+		  }
+		});
+	$("#myModal3").modal("show");
+}
+
+
+
+function showForgotPasswordModal(){
+	$("#myModal3").modal("hide");
+	$("#myModal3").on('hidden.bs.modal', function (event) {
+		  if ($('.modal:visible').length) //check if any modal is open
+		  {
+		    $('body').addClass('modal-open');//add class to body
+		  }
+		});
+	
+	$("#myModal7").modal("show");
+	
+}
